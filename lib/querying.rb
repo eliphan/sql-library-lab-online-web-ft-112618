0d04746049497bb08179dcdbdf6b2efda338b818
Elise Phan <aliceconnic@gmail.com>
2019-01-10 20:36:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name,    WHERE authors.id = books.author_id"
+  "SELECT author_id,subgenre_id FROM series GROUP BY author_id"
 end
 
 def select_series_title_with_most_human_characters
