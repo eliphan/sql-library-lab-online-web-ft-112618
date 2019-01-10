7b693a2ee8584964fa4ff012d84f48e760374205
Elise Phan <aliceconnic@gmail.com>
2019-01-10 19:57:49 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -21,3 +21,10 @@ CREATE TABLE books (
   year TEXT,
   series_id INTEGER
   );
+  
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  series_id INTEGER)
